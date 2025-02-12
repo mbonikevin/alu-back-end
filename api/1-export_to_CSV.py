9997e8777609fa34befa003247044ff6170732ca
mbonikevin <k.mbonimpay@alustudent.com>
2025-02-12 10:16:32 +0200

    documented

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -24,20 +24,21 @@ def gather_data(employee_id):
     data = response.json()
     employee_name = get_employee_name(employee_id)
 
-    tasks = [(employee_id, employee_name, task['completed'],
-              task['title']) for task in data]
+    tasks = [
+        (employee_id, employee_name, task['completed'], task['title'])
+        for task in data
+    ]
 
     file_name = f"{employee_id}.csv"
-    with open(file_name, mode='w', newline='', 
-              encoding='utf-8') as csvfile:
-        csv_writer = csv.writer(csvfile, quotechar='"', 
-                                quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow(["USER_ID", "USERNAME", 
-                             "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+    with open(file_name, mode='w', newline='', encoding='utf-8') as csvfile:
+        csv_writer = csv.writer(csvfile, quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
         csv_writer.writerows(tasks)
 
+    print(
+        f"Data for employee {employee_name} has been exported to {file_name}"
+    )
 
-    print(f"Data for employee {employee_name} has been exported to {file_name}")
 
 def get_employee_name(employee_id):
     """fetches the name of an employee from the API"""
@@ -49,8 +50,10 @@ def get_employee_name(employee_id):
     else:
         return 'Unknown User'
 
+
 if __name__ == "__main__":
     """main function to process command-line arguments and execute gathering"""
+    
     if len(sys.argv) != 2:
         print("Usage: python3 1-export_to_CSV.py <employee_id>")
     else:
