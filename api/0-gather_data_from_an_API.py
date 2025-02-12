7d8f412a4d3392dbd2de32e9abca2853e2509e9c
mbonikevin <k.mbonimpay@alustudent.com>
2025-02-12 09:59:48 +0200

    documented

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""api geting data"""
 
-import sys
-import requests
+import sys  # system operations
+import requests  # HTTP requests
 
 def gather_data(employee_id):
-    """Fetch and display tasks for an employee"""
+    """Fetch and display tasks for an employee."""
     url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
     response = requests.get(url)
     if response.status_code != 200:
@@ -18,16 +17,14 @@ def gather_data(employee_id):
     completed_count = len(completed_tasks)
 
     if total_tasks == 0:
-        print(f"Employee 
-        {employee_name(employee_id)} 
-        is done with tasks(0/0):")
+        print(f"Employee {employee_name(employee_id)} is done with tasks(0/0):")
     else:
         print(f"Employee {employee_name(employee_id)} is done with tasks({completed_count}/{total_tasks}):")
         for task in completed_tasks:
             print(f"\t {task}")
 
 def employee_name(employee_id):
-    """Fetch employee name using their id"""
+    """Fetch employee name using their ID."""
     url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     response = requests.get(url)
     if response.status_code == 200:
